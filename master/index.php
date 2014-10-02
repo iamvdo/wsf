822bac1ed3b2c207acc31942d6a91712fdb876d0
Vincent De Oliveira <vincent@iamvdo.me>
2014-10-02 23:26:38 +0200

    02/10 night

--- master/index.php
+++ master/index.php
@@ -1,5 +1,14 @@
 <?php 
 
-include('../index.html');
-
+if (isset($_POST['login']) and $_POST['login'] == 'vdo' and isset($_POST['pass']) and md5($_POST['pass']) == '7e1b665cee96b023b4da4ee742d273c0') {
+	include('../index.html');
+} else {
  ?>
+ <form action="#" method="post">
+ 	<input type="text" name="login">
+ 	<input type="password" name="pass">
+ 	<input type="submit">
+ </form>
+ <?php 
+ }
+  ?>
